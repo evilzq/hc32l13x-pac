0674301909b968644971123442f38ec825ec7e3f
EvilZQ <evilzq@outlook.com>
2022-07-31 22:58:26 +0800

    修正Windows保留文件名冲突

--- src/wdt.rs
+++ src/wdt.rs
@@ -5,13 +5,13 @@ pub struct RegisterBlock {
     #[doc = "0x80 - desc RST"]
     pub rst: crate::Reg<rst::RST_SPEC>,
     #[doc = "0x84 - desc CON"]
-    pub con: crate::Reg<con::CON_SPEC>,
+    pub con: crate::Reg<con_s::CON_SPEC>,
 }
 #[doc = "RST register accessor: an alias for `Reg<RST_SPEC>`"]
 pub type RST = crate::Reg<rst::RST_SPEC>;
 #[doc = "desc RST"]
 pub mod rst;
 #[doc = "CON register accessor: an alias for `Reg<CON_SPEC>`"]
-pub type CON = crate::Reg<con::CON_SPEC>;
+pub type CON = crate::Reg<con_s::CON_SPEC>;
 #[doc = "desc CON"]
-pub mod con;
+pub mod con_s;
